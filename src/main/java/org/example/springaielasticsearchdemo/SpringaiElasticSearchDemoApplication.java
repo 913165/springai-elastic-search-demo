8239a96ab2aecd23e4ffce7a673847183523e381
Pritesh <tinumistry@gmail.com>
2024-09-23 22:54:50 +0530

    first command

---
+++ src/main/java/org/example/springaielasticsearchdemo/SpringaiElasticSearchDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.springaielasticsearchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiElasticSearchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaiElasticSearchDemoApplication.class, args);
+    }
+
+}
